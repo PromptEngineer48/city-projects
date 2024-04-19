3f94c481c06c98b907f750dfbee4dd599439b536
Prompt Engineer <prompt.engineer48@gmail.com>
2024-04-19 19:04:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,49 +2,42 @@ import streamlit as st
 import requests
 import time
 import os
-
-# Function to get or create session state
-def get_session_state():
-    return st.session_state.setdefault("session_state", {"history": []})
     
 st.title("Saratoga City Code Chatbot")
 
-# Accessing the secret
 api_key = st.secrets["api_key"]
-# st.write("api_key:", api_key)
-
-# Setting environment variable
 os.environ["api_key"] = api_key
 
-query = st.text_input("Enter your query:", "Which city are we talking about?")
+
+
+user_input = st.text_input("Enter your query:", "Which city are we talking about?")
 namespace = st.text_input("Enter the namespace:", "Saratoga_CA")
-system_prompt = st.text_input(
-    "Enter the System Prompt",
-    "Answer the question based on the context below. You are a chatbot designed for this purpose. Retrict your replies to being a city code chatbot only",
-)
+thread_id = st.text_input("Enter the thread_id", "thread_VXoSkmz2GEKqDVIdwCykUUBa")
 
 
-session_state = get_session_state()
-history = session_state["history"]
+# session_state = get_session_state()
+# history = session_state["history"]
 
 # Button to trigger API call
 if st.button("Submit"):
-    if query and namespace:
-        # st.write("Calling API...")
-        url = "https://api.runpod.ai/v2/jtxar0sp6shhwb/run"
+
+    if user_input and namespace and thread_id:
+        st.write("Calling API...")
+
+        url = "https://api.runpod.ai/v2/nysr8clq7it18g/run"
         headers = {
             "Authorization": f"Bearer {api_key}",
             "Content-Type": "application/json",
         }
         payload = {
             "input": {
-                "query": query,
+                "user_input": user_input,
                 "namespace": namespace,
-                "history": history,
-                "system_prompt": system_prompt,
+                "thread_id": thread_id,
             }
         }
-        # st.write(history)
+        # st.write(payload)
+
         response = requests.post(url, json=payload, headers=headers)
 
         if response.status_code == 200:
@@ -62,7 +55,7 @@ if st.button("Submit"):
                 # st.write("Waiting for job to complete...")
                 time.sleep(1)  # Wait for 1 second before checking again
                 status_response = requests.get(
-                    f"https://api.runpod.ai/v2/jtxar0sp6shhwb/status/{job_id}",
+                    f"https://api.runpod.ai/v2/nysr8clq7it18g/status/{job_id}",
                     headers=headers,
                 )
                 if status_response.status_code == 200:
@@ -88,16 +81,16 @@ if st.button("Submit"):
                     st.write("**Bot Answer**")
                     st.write(answer)
 
-                    urls = output[2]
-                    st.write("**URLS:**")
-                    st.write(urls)
+                    references = output[1]
+                    st.write("**References:**")
+                    st.write(references)
 
-                    history.append({"Question": query, "Answer": answer})
-                    history = history[-8:]
-                    session_state["history"] = history  # Update session state
+                    # history.append({"Question": query, "Answer": answer})
+                    # history = history[-8:]
+                    # session_state["history"] = history  # Update session state
 
-                    st.write("**History:**")
-                    st.write(history)
+                    # st.write("**History:**")
+                    # st.write(history)
 
                 else:
                     print("No output found in the response.")
